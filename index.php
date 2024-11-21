e5abc1d3f471429a7a954cd58e3326f0ffdf9bd7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-21 10:47:42 -0600

    update

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-happy place
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARC Platform</title>
+</head>
+<body>
+
+<h1>
+    ARC
+</h1>
+<p>
+    Audit and Reporting Console
+</p>
+    
+</body>
+</html>
