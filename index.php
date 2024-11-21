7102706f4a8ea8a7506ce91e8f076e705f966ce4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-21 16:38:24 -0600

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - ARC Platform</title>
-    <link rel="stylesheet" href="assets/css/login_style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/css/login_styles.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <div class="login-container">
