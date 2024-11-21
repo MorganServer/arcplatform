447e694dcf491cc1d4fd4c3d3b5971db285a561a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-21 16:33:41 -0600

    update

--- index.php
+++ index.php
@@ -3,41 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>Dashbaord - ARC Platform</title>
+    <title>Login - ARC Platform</title>
 </head>
 <body>
-
-<div class="header" style="overflow: auto; margin-bottom: 5px;">
-    <div style="float: left;">
-        <img src="assets/images/logo.png" width="250" alt="">
-    </div>
-    <div style="float: right;">
-        <div class="nav">
-            <ul class="nav-list">
-                <li class="nav-list-item"><a class="nav-list-item-link" href="">Dashboard</a></li>
-                <li class="nav-list-item"><a class="nav-list-item-link" href="">Engagements</a></li>
-                <li class="nav-list-item"><a class="nav-list-item-link" href="">Clients</a></li>
-                <li class="nav-list-item"><a class="nav-list-item-link" href="">Settings</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-
-<div class="page_header">
-    <div class="left">
-        <h4>Dashboard</h4>
-    </div>
-    <div class="right">
-        <p style="padding-right: 15px;">Welcome, Garrett Morgan</p>
-        <a class="header-icon" href=""><i class="bi bi-info-circle-fill"></i></a>
-        <a class="header-icon" href=""><i class="bi bi-lock-fill"></i></a>
-        <a class="header-icon" href=""><i class="bi bi-box-arrow-right"></i></a>
-    </div>
-</div>
-
-
     
+    
+
 </body>
 </html>
