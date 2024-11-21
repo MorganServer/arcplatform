f728941e7ecc5c3a2f1d777304272f1db669b414
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-21 17:00:10 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -43,11 +43,10 @@
     <div class="row">
         <div class="card" style="width: 18rem;">
           <div class="card-body">
+            <p style="font-size: 25x;">
+                25
+            </p>
             <h5 class="card-title">Open Engagements</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
           </div>
         </div>
         <div class="card">
