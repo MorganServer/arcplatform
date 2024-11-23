d86bd0277e4e9a1b604162d7f68767d1303d598f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-22 22:38:28 -0600

    update

--- engagements/index.php
+++ engagements/index.php
@@ -85,17 +85,17 @@ redirectIfNotLoggedIn();
                     <td><?php echo $year ? $year : '-'; ?></td>
                     <td><?php echo $engagement_type ? $engagement_type : '-'; ?></td>
                     <!-- <td><?php //echo $status ? $status : '-'; ?></td> -->
-                    <td style="width: 30px; text-align: center;">
+                    <td style="width: 45px; text-align: center;">
                         <!-- <a href="<?php //echo BASE_URL; ?>/asset/view/?id=<?php //echo $id; ?>" class="view"> -->
                             <i class="bi bi-eye text-success"></i>
                         </a> 
                     </td>
-                    <td style="width: 30px; text-align: center;">
+                    <td style="width: 45px; text-align: center;">
                         <!-- <a href="<?php //echo BASE_URL; ?>/asset/update/?id=<?php //echo $id; ?>"> -->
                             <i class="bi bi-pencil-square" style="color:#005382;"></i>
                         </a> 
                     </td>
-                    <td style="width: 30px; text-align: center;">
+                    <td style="width: 45px; text-align: center;">
                         <!-- <a href="<?php //echo BASE_URL; ?>/asset/delete/?id=<?php //echo $id; ?>" class="delete"> -->
                             <i class="bi bi-trash" style="color:#941515;"></i>
                         </a>
