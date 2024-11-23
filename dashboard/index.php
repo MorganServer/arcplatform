81911d59ac9c908e1cdb7f8c78ed832878d2ef37
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-22 22:57:15 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -34,56 +34,118 @@ redirectIfNotLoggedIn();
 
 
 
-<div class="content pt-5 d-flex">
-    <div class="row mx-auto">
-                    <div class="card" style="border-bottom: 3px solid brown; border-radius: 5px !important;">
-                        <a class="text-decoration-none text-black stretched-link" href="<?php echo BASE_URL; ?>/pages/servers.php">
-                        <div class="card-body text-center">
-                            <p class="card-text">
-                                <div class="card_text_left float-start" style="font-size: 45px;">
-                                    <i class="bi bi-hdd-stack ps-4"></i>
-                                </div>
-                                <div class="card_text_right float-end pe-3">
-                                    <h2 class="text-end">
-                                        <?php
-                                            $sql="SELECT count('1') FROM engagement WHERE status='Open'";
-                                            $result=mysqli_query($conn,$sql);
-                                            $rowtotal=mysqli_fetch_array($result); 
-                                            if($rowtotal[0] < 10) {
-                                                echo "0$rowtotal[0]";
-                                            } else {
-                                                echo "$rowtotal[0]";
-                                            }
-                                        ?>
-                                    </h2>
-                                    <p class="text-muted text-end">Servers</p>
-                                </div>
-                            </p>
-                        </div>
-                        </a>
+    <div class="content pt-5 d-flex">
+
+        <!-- Top Card Container -->
+            <div class="card-container">
+                <div class="card" style="border-bottom: 3px solid brown; border-radius: 5px !important;">
+                    <a class="text-decoration-none text-black stretched-link" href="<?php echo BASE_URL; ?>/pages/servers.php">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                            <div class="card_text_left float-start" style="font-size: 45px;">
+                                <i class="bi bi-collection-fill ps-4"></i>
+                            </div>
+                            <div class="card_text_right float-end pe-3">
+                                <h2 class="text-end">
+                                    <?php
+                                        $sql="SELECT count('1') FROM engagement WHERE status='Open'";
+                                        $result=mysqli_query($conn,$sql);
+                                        $rowtotal=mysqli_fetch_array($result); 
+                                        if($rowtotal[0] < 10) {
+                                            echo "0$rowtotal[0]";
+                                        } else {
+                                            echo "$rowtotal[0]";
+                                        }
+                                    ?>
+                                </h2>
+                                <p class="text-muted text-end">Engagements</p>
+                            </div>
+                        </p>
                     </div>
-        <div class="card me-5" style="width: 18rem;">
-          <div class="card-body text-center">
-            <p style="font-size: 35px;">25</p>
-            <h5 class="card-title text-secondary">Open Engagements</h5>
-          </div>
-        </div>
-
-        <div class="card me-5" style="width: 18rem;">
-          <div class="card-body text-center">
-            <p style="font-size: 35px;">25</p>
-            <h5 class="card-title text-secondary">Open QA Comments</h5>
-          </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-          <div class="card-body text-center">
-            <p style="font-size: 35px;">25</p>
-            <h5 class="card-title text-secondary">Tasks</h5>
-          </div>
-        </div>
+                    </a>
+                </div>
+                <div class="card" style="border-bottom: 3px solid gray; border-radius: 5px !important;">
+                    <a class="text-decoration-none text-black stretched-link" href="<?php echo BASE_URL; ?>/pages/computers.php">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                            <div class="card_text_left float-start" style="font-size: 45px;">
+                                <i class="bi bi-people-fill ps-4"></i>
+                            </div>
+                            <div class="card_text_right float-end pe-3">
+                                <h2 class="text-end">
+                                    <?php
+                                        $sql="SELECT count('1') FROM client";
+                                        $result=mysqli_query($conn,$sql);
+                                        $rowtotal=mysqli_fetch_array($result); 
+                                        if($rowtotal[0] < 10) {
+                                            echo "0$rowtotal[0]";
+                                        } else {
+                                            echo "$rowtotal[0]";
+                                        }
+                                    ?>
+                                </h2>
+                                <p class="text-muted text-end">Clients</p>
+                            </div>
+                        </p>
+                    </div>
+                    </a>
+                </div>
+                <div class="card" style="border-bottom: 3px solid purple; border-radius: 5px !important;">
+                    <a class="text-decoration-none text-black stretched-link" href="<?php echo BASE_URL; ?>/pages/network.php">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                            <div class="card_text_left float-start" style="font-size: 45px;">
+                                <i class="bi bi-diagram-2 ps-4"></i>
+                            </div>
+                            <div class="card_text_right float-end pe-3">
+                                <h2 class="text-end">
+                                    <?php
+                                        $sql="SELECT count('1') FROM qa_comments WHERE status='Open'";
+                                        $result=mysqli_query($conn,$sql);
+                                        $rowtotal=mysqli_fetch_array($result); 
+                                        if($rowtotal[0] < 10) {
+                                            echo "0$rowtotal[0]";
+                                        } else {
+                                            echo "$rowtotal[0]";
+                                        }
+                                    ?>
+                                </h2>
+                                <p class="text-muted text-end">QA Comments Open</p>
+                            </div>
+                        </p>
+                    </div>
+                    </a>
+                </div>
+                <!-- <div class="card" style="border-bottom: 3px solid orange; border-radius: 5px !important;">
+                    <a class="text-decoration-none text-black stretched-link" href="<?php //echo BASE_URL; ?>/pages/mobile.php">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                            <div class="card_text_left float-start" style="font-size: 45px;">
+                                <i class="bi bi-phone ps-4"></i>
+                            </div>
+                            <div class="card_text_right float-end pe-3">
+                                <h2 class="text-end">
+                                    <?php
+                                        // $sql="SELECT count('1') FROM assets WHERE asset_type='Mobile Device'";
+                                        // $result=mysqli_query($conn,$sql);
+                                        // $rowtotal=mysqli_fetch_array($result); 
+                                        // if($rowtotal[0] < 10) {
+                                        //     echo "0$rowtotal[0]";
+                                        // } else {
+                                        //     echo "$rowtotal[0]";
+                                        // }
+                                    ?>
+                                </h2>
+                                <p class="text-muted text-end">Mobile Devices</p>
+                            </div>
+                        </p>
+                    </div>
+                    </a>
+                </div> -->
+            </div>
+        <!-- end Top Card Container -->
+    
     </div>
-</div>
 
 </body>
 </html>
