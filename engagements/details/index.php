3e578a1e68e3345cba06f6c51b352adc88551b04
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-23 09:24:42 -0600

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -78,7 +78,6 @@ redirectIfNotLoggedIn();
 
             <div class="card-container">
                 <div class="card" style="width: 20rem;">
-                  <img src="..." class="card-img-top" alt="...">
                   <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -86,7 +85,6 @@ redirectIfNotLoggedIn();
                   </div>
                 </div>
                 <div class="card" style="width: 20rem;">
-                  <img src="..." class="card-img-top" alt="...">
                   <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -94,7 +92,6 @@ redirectIfNotLoggedIn();
                   </div>
                 </div>
                 <div class="card" style="width: 35rem;">
-                  <img src="..." class="card-img-top" alt="...">
                   <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
