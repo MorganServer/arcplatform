f853d5577086dab87cebafbb021707fcf49137ff
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-22 21:51:03 -0600

    update

--- app/includes/sub_header.php
+++ app/includes/sub_header.php
@@ -3,7 +3,7 @@
         <h6>Dashboard</h6>
     </div>
     <div class="right">
-        <p style="padding-right: 15px;">Welcome, <?php echo $_SESSION['full_name']; ?></p>
+        <p style="padding-right: 15px;">Welcome, <?php echo $_SESSION['email']; ?></p>
         <a class="header-icon" href=""><i class="bi bi-info-circle-fill"></i></a>
         <a class="header-icon" href=""><i class="bi bi-lock-fill"></i></a>
         <a class="header-icon" href="?logout=1"><i class="bi bi-box-arrow-right"></i></a>
