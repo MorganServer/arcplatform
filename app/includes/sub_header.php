04724a3a6700fe4b05e4dfcadcf7be81bd987106
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-22 21:31:21 -0600

    update

--- app/includes/sub_header.php
+++ app/includes/sub_header.php
@@ -6,6 +6,6 @@
         <p style="padding-right: 15px;">Welcome, Garrett Morgan</p>
         <a class="header-icon" href=""><i class="bi bi-info-circle-fill"></i></a>
         <a class="header-icon" href=""><i class="bi bi-lock-fill"></i></a>
-        <a class="header-icon" href="<?php echo BASE_URL . "/?logout=1"; ?>"><i class="bi bi-box-arrow-right"></i></a>
+        <a class="header-icon" href="<?php echo BASE_URL . "/?logout=1" ?>"><i class="bi bi-box-arrow-right"></i></a>
     </div>
 </div>
