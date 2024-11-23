cc92518dc147816f3e03d89107c289430e6c73c6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-22 22:01:47 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,6 +1,6 @@
 <div class="header" style="overflow: auto; margin-bottom: 5px;">
     <div style="float: left; padding-left: 20px;">
-        <img src="../assets/images/logo.png" width="250" alt="">
+        <img src="../assets/images/logo.png" width="300" alt="">
     </div>
     <div style="float: right;">
         <div class="nav">
