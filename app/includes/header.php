14b411569b9abc6902efe87d422e9bec82d60e15
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-23 09:02:38 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,6 +1,6 @@
 <div class="header" style="overflow: auto; margin-bottom: 5px;">
     <div style="float: left; padding-left: 20px;">
-        <a href="<?php BASE_URL; ?>/"><img src="../assets/images/logo.png" width="300" alt=""></a>
+        <a href="<?php BASE_URL; ?>/"><img src="<?php ROOT_PATH; ?>/assets/images/logo.png" width="300" alt=""></a>
     </div>
     <div style="float: right;">
         <div class="nav">
