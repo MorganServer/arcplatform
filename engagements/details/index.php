3963d559d213a45b05c03e529ddd8bc18a343846
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-23 08:39:16 -0600

    update

---
+++ engagements/details/index.php
@@ -0,0 +1,74 @@
+<?php
+date_default_timezone_set('America/Denver');
+require_once "../app/database/connection.php";
+require_once "../path.php";
+session_start();
+
+$files = glob("../app/functions/*.php");
+foreach ($files as $file) {
+    require_once $file;
+}
+
+// Trigger the logout function
+if (isset($_GET['logout']) && $_GET['logout'] == 1) {
+    logoutUser($conn);
+}
+
+redirectIfNotLoggedIn();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/styles.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Engagement Details - ARC Platform</title>
+</head>
+<body>
+
+    <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+    <?php include(ROOT_PATH . "/app/includes/sub_header.php"); ?>
+
+    <!-- main-container -->
+        <div class="container" style="padding: 0 5px 0 5px;">
+            <h2 class="mt-4">
+                Engagement Details
+            </h2>
+            <hr>
+
+            <!-- php code for getting asset details -->
+            <?php
+            $id = $_GET['id'];
+            $off_sql = "SELECT * FROM engagements WHERE engagement_id = $id";
+            $off_result = mysqli_query($conn, $off_sql);
+            if($off_result) {
+            $num_rows = mysqli_num_rows($off_result);
+            if($num_rows > 0) {
+                while ($off_row = mysqli_fetch_assoc($off_result)) {
+                    $off_id                     = $off_row['engagement_id']; 
+                    $off_client_name             = $off_row['client_name']; 
+                    
+
+                    $today = date('Y-m-d');
+                    $is_today = ($off_audit_schedule == $today) ? true : false;
+                }
+            // }}
+            ?>
+        <!-- end php code for getting asset details -->
+
+        <?php echo $off_client_name; ?>
+
+
+        <?php }
+        } ?>
+
+        </div>
+    <!-- END main-container -->
+
+
+
+</body>
+</html>
