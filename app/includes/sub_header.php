1ec61c2dd5c35ada8fdabef30e20681952d1b358
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-22 21:53:11 -0600

    update

--- app/includes/sub_header.php
+++ app/includes/sub_header.php
@@ -1,9 +1,13 @@
+<?php
+$currentPageName = basename(__DIR__);
+?>
 <div class="page_header">
     <div class="left">
-        <h6>Dashboard</h6>
+    <h6><?php echo ucfirst($currentPageName); ?></h6>
+
     </div>
     <div class="right">
-        <p style="padding-right: 15px;">Welcome, <?php echo $_SESSION['email']; ?></p>
+        <p style="padding-right: 15px;">Welcome, <?php echo $_SESSION['full_name']; ?></p>
         <a class="header-icon" href=""><i class="bi bi-info-circle-fill"></i></a>
         <a class="header-icon" href=""><i class="bi bi-lock-fill"></i></a>
         <a class="header-icon" href="?logout=1"><i class="bi bi-box-arrow-right"></i></a>
