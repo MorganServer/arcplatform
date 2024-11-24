90ad0e4785e8328fe4fee4a6ac8a71e981bf5d87
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-23 22:46:48 -0600

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -26,6 +26,12 @@ redirectIfNotLoggedIn();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Engagement Details - ARC Platform</title>
+
+    <style>
+        .qa-comment-row:hover {
+            background-color: #f8f9fa; /* Light gray for hover effect */
+        }
+    </style>
 </head>
 <body>
 
