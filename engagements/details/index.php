c7be6fab6abe4b032d3cfd74b670b6a6e35019ed
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-24 17:10:39 -0600

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['followup_owner'])) {
 
     if ($qaId && !empty($comment) && !empty($owner)) {
         // Insert the comment into the database
-        $stmt = $conn->prepare("INSERT INTO followup_comments (qa_id, comment, owner) VALUES (?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO followup_comments (qa_id, followup_comment, followup_owner) VALUES (?, ?, ?)");
         $stmt->bind_param("iss", $qaId, $comment, $owner);
 
         if ($stmt->execute()) {
