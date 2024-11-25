ebc6378ccc2bea08b373ce57aa5b8ed03a2ff5c2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-25 17:58:01 -0600

    update

--- qa_comments/index.php
+++ qa_comments/index.php
@@ -101,7 +101,16 @@ redirectIfNotLoggedIn();
                                 <td><?php echo $control_ref ? $control_ref : '-'; ?></td>
                                 <td><?php echo $cell_ref ? $cell_ref : '-'; ?></td>
                                 <td><?php echo $comment_by ? $comment_by : '-'; ?></td>
-                                <td><?php echo $status ? $status : '-'; ?></td>
+                                <td>
+                                    <?php if($status == 'New') { ?>
+                                        <span class="badge" style="background-color: #ecf4f9; color: #2d60a3;"><?php echo $status ? $status : '-'; ?></span>
+                                    <?php } else if($status == 'Follow-Up') { ?>
+                                        <span class="badge" style="background-color: #fdf1e0; color: #785524;"><?php echo $status ? $status : '-'; ?></span>
+                                    <?php } else if($status == 'Completed') { ?>
+                                        <span class="badge" style="background-color: #e9f8e3; color: #497a37;"><?php echo $status ? $status : '-'; ?></span>
+                                    <?php } ?>
+                                    
+                                </td>
                                 <td style="width: 100px; text-align: center;">
                                     <a href="<?php echo BASE_URL; ?>/engagements/details/?id=<?php echo $engage_id; ?>" class="view">
                                         <i class="bi bi-eye text-success"></i>
