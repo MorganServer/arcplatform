d60773d29bb2a3e5c4fb7e3ed86450a7400625e4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-24 20:12:17 -0600

    update

--- app/includes/sub_header.php
+++ app/includes/sub_header.php
@@ -12,7 +12,7 @@ $pageName = ucfirst($currentDirectory);
     </div>
     <div class="right">
         <p style="padding-right: 15px;">Welcome, <?php echo $_SESSION['full_name']; ?></p>
-        <a class="header-icon" data-bs-toggle="modal" data-bs-target="#add_content"><i class="bi bi-plus-circle-fill"></i></a>
+        <a class="header-icon" data-bs-toggle="modal" data-bs-target="#add_content" style="cursor: pointer;"><i class="bi bi-plus-circle-fill"></i></a>
         <a class="header-icon" href="?logout=1"><i class="bi bi-box-arrow-right"></i></a>
     </div>
 </div>
@@ -24,26 +24,17 @@ $pageName = ucfirst($currentDirectory);
 <div class="modal fade" id="add_content" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
-      </div>
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+
+            <a class="btn btn-primary" href="<?php BASE_URL; ?>/actions/add_qa_comment/" role="button">Add QA Comment</a>
+            <a class="btn btn-primary" href="<?php BASE_URL; ?>/actions/add_client/" role="button">Add Client</a>
+            <a class="btn btn-primary" href="<?php BASE_URL; ?>/actions/add_engagement/" role="button">Add Engagement</a>
+
+        </div>
     </div>
   </div>
 </div>
