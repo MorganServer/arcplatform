ed245fa067b4d33d8b07704c55d096f50bb7d755
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-25 08:54:32 -0600

    update

--- clients/index.php
+++ clients/index.php
@@ -91,7 +91,7 @@ redirectIfNotLoggedIn();
                                     <td>
                                         <?php
                                             // Counting open QA comments for the client
-                                            $sql_qa = "SELECT COUNT(*) FROM qa_comments WHERE client_name='$client_name' AND status != 'Completed'";
+                                            $sql_qa = "SELECT COUNT(*) FROM qa_comments WHERE client_name='$client_name' AND status!='Completed'";
                                             $result_qa = mysqli_query($conn, $sql_qa);
                                             if ($result_qa) {
                                                 $rowtotal = mysqli_fetch_array($result_qa);
