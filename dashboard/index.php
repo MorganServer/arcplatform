72e26b1a2abf118317f5d69bd90e06d5027945de
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-24 20:09:20 -0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -150,5 +150,7 @@ redirectIfNotLoggedIn();
         </div>
 
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
