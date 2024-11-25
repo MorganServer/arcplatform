114b194e6fe8fd1a9d1bdcf833200ba2999cc5fc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-24 20:17:27 -0600

    update

--- actions/add_client/index.php
+++ actions/add_client/index.php
@@ -1,10 +1,10 @@
 <?php
 date_default_timezone_set('America/Denver');
-require_once "../app/database/connection.php";
-require_once "../path.php";
+require_once "../../app/database/connection.php";
+require_once "../../path.php";
 session_start();
 
-$files = glob("../app/functions/*.php");
+$files = glob("../../app/functions/*.php");
 foreach ($files as $file) {
     require_once $file;
 }
@@ -22,7 +22,7 @@ redirectIfNotLoggedIn();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/styles.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../assets/css/styles.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Add Client - ARC Platform</title>
