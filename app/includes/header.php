bbec867f7d4c10a3301f581e37269e64e1d6c4cd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 03:10:16 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -2,14 +2,4 @@
     <div style="float: left; padding-left: 20px;">
         <a href="<?php BASE_URL; ?>/"><img src="<?php ROOT_PATH; ?>/assets/images/logo.png" width="300" alt=""></a>
     </div>
-    <div style="float: right;">
-        <div class="nav">
-            <ul class="nav-list">
-                <li class="nav-list-item"><a class="nav-list-item-link" href="<?php BASE_URL; ?>/">Dashboard</a></li>
-                <li class="nav-list-item"><a class="nav-list-item-link" href="<?php BASE_URL; ?>/engagements">Engagements</a></li>
-                <li class="nav-list-item"><a class="nav-list-item-link" href="<?php BASE_URL; ?>/clients">Clients</a></li>
-                <li class="nav-list-item"><a class="nav-list-item-link" href="<?php BASE_URL; ?>/settings">Settings</a></li>
-            </ul>
-        </div>
-    </div>
 </div>
