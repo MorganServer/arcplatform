4021134a735e1dbaf9921964d9e0cb4e828789a6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 01:44:22 -0600

    update

--- client_list/details/index.php
+++ client_list/details/index.php
@@ -95,7 +95,7 @@ redirectIfNotLoggedIn();
             </div>
 
 
-            <div class="accordion accordion-flush" id="accordionFlushExample" style="background-color: blue;">
+            <div class="accordion accordion-flush" id="accordionFlushExample" style="background-color: blue !important;">
               <div class="accordion-item">
                 <h2 class="accordion-header">
                   <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
