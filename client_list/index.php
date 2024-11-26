c2376d75517c8f89620408a99ea6220aefaee403
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 03:11:14 -0600

    update

--- client_list/index.php
+++ client_list/index.php
@@ -198,5 +198,7 @@ redirectIfNotLoggedIn();
         </div>
     <!-- END main-container -->
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
