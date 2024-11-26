55c70ff0c2d7a1d63cf07b60e1ffdc8b5188babd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 10:46:28 -0600

    update

--- app/fetch_client_data.php
+++ app/fetch_client_data.php
@@ -1,24 +1,28 @@
 <?php
-// fetch_client_data.php
+// Include your database connection
+include(ROOT_PATH . '/app/database/connection.php');
 
-
-// Check if the client_id is passed
+// Check if the client_id is passed via GET request
 if (isset($_GET['client_id'])) {
     $client_id = $_GET['client_id'];
-    
-    // Query the database for the client data
+
+    // Query to get client details based on client_id
     $sql = "SELECT * FROM clients WHERE client_id = '$client_id'";
     $result = mysqli_query($conn, $sql);
 
-    if ($result) {
+    // Check if the query is successful
+    if ($result && mysqli_num_rows($result) > 0) {
         $client = mysqli_fetch_assoc($result);
+
+        // Return client data as JSON
         echo json_encode([
             'client_name' => $client['client_name'],
             'primary_contact' => $client['primary_contact'],
             'contact_email' => $client['contact_email'],
-            'has_logo' => $client['logo'] // Assuming logo column is 1 if the client has a logo
+            'has_logo' => $client['logo'] // Assuming 'logo' column is 1 if client has logo
         ]);
     } else {
+        // Return an error if no client is found
         echo json_encode(['error' => 'Client not found']);
     }
 } else {
