a56e6e0fdb4ef86c0c00ea9f9d0b8b8c5cc2d18a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 09:48:32 -0600

    update

--- app/includes/sub_header.php
+++ app/includes/sub_header.php
@@ -265,7 +265,7 @@ $pageName = ucwords($pageName);
 <!-- end add-qa-comment -->
 
 <!-- manage-client -->
-    <div class="modal fade" id="delete_clients" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="manage_clients" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -320,12 +320,12 @@ $pageName = ucwords($pageName);
     <?php
     if (isset($_GET['action']) && $_GET['action'] === 'delete' && isset($_GET['dc_id'])) {
         $dc_id = intval($_GET['dc_id']); // Sanitize the input to prevent SQL injection
-    
+
         // Prepare the SQL query
         $sql = "DELETE FROM clients WHERE dc_id = ?";
         if ($stmt = $conn->prepare($sql)) {
             $stmt->bind_param("i", $dc_id);
-        
+
             // Execute the statement
             if ($stmt->execute()) {
                 // Redirect back to the same page after successful deletion
@@ -334,7 +334,7 @@ $pageName = ucwords($pageName);
             } else {
                 echo "<div class='alert alert-danger'>Error deleting client: " . $stmt->error . "</div>";
             }
-        
+
             $stmt->close();
         } else {
             echo "<div class='alert alert-danger'>Error preparing the statement: " . $conn->error . "</div>";
