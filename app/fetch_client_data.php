2b554c6f4b091618152e6275010564f46583b011
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 10:48:55 -0600

    update

--- app/fetch_client_data.php
+++ app/fetch_client_data.php
@@ -6,6 +6,9 @@ include(ROOT_PATH . '/app/database/connection.php');
 if (isset($_GET['client_id'])) {
     $client_id = $_GET['client_id'];
 
+    // Debug: Log the client_id received in the GET request
+    error_log("Received client_id: " . $client_id);
+
     // Query to get client details based on client_id
     $sql = "SELECT * FROM clients WHERE client_id = '$client_id'";
     $result = mysqli_query($conn, $sql);
@@ -13,6 +16,9 @@ if (isset($_GET['client_id'])) {
     // Check if the query is successful
     if ($result && mysqli_num_rows($result) > 0) {
         $client = mysqli_fetch_assoc($result);
+
+        // Debug: Log the fetched client data
+        error_log("Fetched client data: " . print_r($client, true));
 
         // Return client data as JSON
         echo json_encode([
@@ -22,10 +28,15 @@ if (isset($_GET['client_id'])) {
             'has_logo' => $client['logo'] // Assuming 'logo' column is 1 if client has logo
         ]);
     } else {
-        // Return an error if no client is found
+        // Log an error if no client is found
+        error_log("No client found with ID: " . $client_id);
+
         echo json_encode(['error' => 'Client not found']);
     }
 } else {
+    // Log an error if client_id is not provided
+    error_log("Client ID not provided");
+
     echo json_encode(['error' => 'Client ID not provided']);
 }
 ?>
