92fc4045dd7a5e390e7bdf937ef1bc2e5762680b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-25 23:55:27 -0600

    update

--- client_list/index.php
+++ client_list/index.php
@@ -33,7 +33,7 @@ redirectIfNotLoggedIn();
     <?php include(ROOT_PATH . "/app/includes/sub_header.php"); ?>
 
     <!-- main-container -->
-    <!-- <div class="container" style="padding: 0 5px 0 5px;"> -->
+    <div class="" style="padding: 0 5px 0 5px;">
             <h2 class="mt-4">
                 Client List
             </h2>
@@ -150,7 +150,7 @@ redirectIfNotLoggedIn();
             echo '</ul>';
         ?>
 
-        <!-- </div> -->
+        </div>
     <!-- END main-container -->
 
 </body>
