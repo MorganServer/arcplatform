130d1070d520f58baf11fde077eb957a83a7efe2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 01:05:19 -0600

    update

--- client_list/index.php
+++ client_list/index.php
@@ -87,8 +87,8 @@ redirectIfNotLoggedIn();
                                     <?php } else {
                                         $first_letter = strtoupper(substr($client_name, 0, 1));
                                     ?>
-                                    <div class="d-flex">
-                                        <div class="align-items-center me-2" style="background-color: <?php echo $random_color; ?>; width: 50px; height: 50px; border-radius: 50%; display: flex; justify-content: center; align-items: center; font-size: 24px; color: white;">
+                                    <div class="d-flex align-items-center">
+                                        <div class="me-2" style="background-color: <?php echo $random_color; ?>; width: 50px; height: 50px; border-radius: 50%; display: flex; justify-content: center; align-items: center; font-size: 24px; color: white;">
                                             <?php echo $first_letter; ?>
                                         </div>
                                         <span><?php echo $client_name ? $client_name : '-'; ?></span>
