856c2efa9e04a33470cb39827b58f2daa2061ac3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 02:19:25 -0600

    update

--- client_list/details/index.php
+++ client_list/details/index.php
@@ -151,10 +151,10 @@ redirectIfNotLoggedIn();
                                                             </div>
                                                                 
                                                             <!-- Right section for the text details -->
-<div class="right ms-3 d-flex flex-column text-start">
-    <h5 class="card-title"><?php echo $off_engagement_type; ?></h5>
-    <h6 class="card-subtitle mb-2 text-muted"><?php echo $off_client_name; ?> - <?php echo $off_year; ?></h6>
-    <p class="card-text">
+<div class="right ms-3 d-flex flex-column align-items-start">
+    <h5 class="card-title text-start"><?php echo $off_engagement_type; ?></h5>
+    <h6 class="card-subtitle mb-2 text-muted text-start"><?php echo $off_client_name; ?> - <?php echo $off_year; ?></h6>
+    <p class="card-text text-start">
         Report Period: <?php echo $formatted_start; ?> to <?php echo $formatted_end; ?><br>
         Report As Of: <?php echo $formatted_as_of; ?>
     </p>
