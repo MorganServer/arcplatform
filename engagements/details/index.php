42686eeef9a5accc3975a7e92724336f9bd50c02
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-25 18:11:55 -0600

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -255,8 +255,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['followup_owner'])) {
                             QA Comment Report
                         </h6>
                         <div class="btn-group mt-2">
-                          <button type="button" class="btn btn-outline-primary"><i class="bi bi-download"></i> Comment Report</button>
+                          <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#download_modal"><i class="bi bi-download"></i> Comment Report</button>
                         </div>
+
+                        <!-- MODAL -->
+
+                            <div class="modal fade" id="download_modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div class="modal-dialog">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                    ...
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+
+                        <!-- end MODAL -->
                         
                     </p>
                   </div>
