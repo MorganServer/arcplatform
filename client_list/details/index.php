ef8031e25021856cc2c4504b0ef5e1e5597ff191
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 02:36:19 -0600

    update

--- client_list/details/index.php
+++ client_list/details/index.php
@@ -163,7 +163,9 @@ redirectIfNotLoggedIn();
                                                                     <?php } else { ?>
                                                                         <span class="text-secondary"><strong>Audit Period:&nbsp;&nbsp;</strong></span>As of <?php echo $formatted_as_of; ?>
                                                                     <?php } ?> <br>
+                                                                    <div class="pt-3"></div>
                                                                     <span class="text-secondary"><strong>Fieldwork Week:&nbsp;&nbsp;</strong></span><?php echo $formatted_fw_week; ?> <br>
+                                                                    <div class="pt-3"></div>
                                                                     <span class="text-secondary"><strong>Fieldwork Week:&nbsp;&nbsp;</strong></span><?php echo $formatted_leadsheets_due; ?>
                                                                 </p>
                                                             </div>
