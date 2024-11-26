83c0e30dfd806204a850683bf2b0c5156ad02fad
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 01:50:37 -0600

    update

--- client_list/details/index.php
+++ client_list/details/index.php
@@ -139,7 +139,7 @@ redirectIfNotLoggedIn();
 
                         <div class="card" style="width: 30rem;">
                           <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title"><?php echo $off_engagement_type; ?></h5>
                             <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="card-link">Card link</a>
