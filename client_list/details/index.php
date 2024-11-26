85a64a3dc8925db9e20162d3f991184c5863b9bd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 02:00:14 -0600

    update

--- client_list/details/index.php
+++ client_list/details/index.php
@@ -103,7 +103,9 @@ redirectIfNotLoggedIn();
                 <div class="accordion-item">
                     <h2 class="accordion-header">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Active Engagements
+                            <h3>
+                                Active Engagements   
+                            </h3>
                         </button>
                     </h2>
                     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
