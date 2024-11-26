5b4a8e0d09596fff5c0176fc1a16cbd316c5eee8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-25 18:05:51 -0600

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -251,6 +251,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['followup_owner'])) {
                   <div class="card-body">
                     <h5 class="card-title">Engagements Resources</h5>
                     <p class="card-text">
+                        <h6>
+                            QA Comment Report
+                        </h6>
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-danger">Action</button>
+                          <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="visually-hidden">Toggle Dropdown</span>
+                          </button>
+                          <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                          </ul>
+                        </div>
                         
                     </p>
                   </div>
