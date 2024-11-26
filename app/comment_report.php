e95314ad78d27cd2877de3b886cfe1e82ec89557
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-26 02:59:37 -0600

    update

--- app/comment_report.php
+++ app/comment_report.php
@@ -5,11 +5,11 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Define constants for root path and base URL
-define("ROOT_PATH", realpath(dirname(__FILE__, 2)));
+// define("ROOT_PATH", realpath(dirname(__FILE__, 2)));
 
 // Include necessary files
-require(ROOT_PATH . '/app/fpdf/fpdf.php'); // Include FPDF library
-require(ROOT_PATH . '/app/database/connection.php'); // Include your database connection file
+require(BASE_URL . '/app/fpdf/fpdf.php'); // Include FPDF library
+require(BASE_URL . '/app/database/connection.php'); // Include your database connection file
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
