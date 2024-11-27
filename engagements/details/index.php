2cecca89c74a2f6d54083219188f26557343d606
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-11-27 16:45:49 -0600

    update

--- engagements/details/index.php
+++ engagements/details/index.php
@@ -2,6 +2,7 @@
 date_default_timezone_set('America/Denver');
 require_once "../../app/database/connection.php"; // Ensure this is correct
 require_once "../../path.php";
+require_once "../../app/functions/add_actions.php";
 
 session_start();
 
